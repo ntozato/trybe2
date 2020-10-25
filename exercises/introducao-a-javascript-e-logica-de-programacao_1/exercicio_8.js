fd494a460fa3360ae501723ba22471867b80b1de
Nícolas Tozato <ntozato9@gmail.com>
2020-10-24 23:23:31 -0300

    exercicio 8

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio_8.js
@@ -0,0 +1,13 @@
+let n1 = 11;
+let n2 = 15;
+let n3 = 21;
+
+if (n1 % 2 === 0) {
+    console.log("true");
+} else if (n2 % 2 === 0) {
+    console.log("true");
+} else if (n3 % 2 === 0) {
+    console.log("true");
+} else {
+    console.log("false");
+}
