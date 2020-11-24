d16b33e4c9bfc76eec951b213de26d1c7c37cc85
Nícolas Tozato <ntozato9@gmail.com>
2020-11-24 18:02:43 -0300

    teste

---
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_1/exercise2_4.js
@@ -0,0 +1,12 @@
+let string = 'Tryber x aqui';
+let nome = 'bebeto';
+let resultado = '';
+
+function changeX(nome) {
+    
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] === x)
+    }
+
+}
+
