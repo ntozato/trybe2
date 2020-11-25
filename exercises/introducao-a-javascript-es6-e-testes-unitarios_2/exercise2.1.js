103abda96bc5d2f296d0309556fda67fcd83b8e0
Nícolas Tozato <ntozato9@gmail.com>
2020-11-25 13:06:44 -0300

    segunda parte exercicios

--- exercises/introducao-a-javascript-es6-e-testes-unitarios_2/exercise2.1.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_2/exercise2.1.js
@@ -18,6 +18,8 @@ const lesson1 = {
     turno: 'noite',
   };
 
+  //part 1
+
   const addNewKey = (obj, key, value) => {
 
 obj[key] = value;
@@ -26,4 +28,46 @@ obj[key] = value;
 
   addNewKey(lesson2, 'turno', 'manhã');
 
-  console.log(lesson2);
+//   console.log(lesson2);
+
+  //part 2
+
+  const keys = (obj) => Object.keys(obj);
+//   console.log(keys(lesson3));
+
+  //part 3
+
+  const objLength = (obj) => Object.keys(obj).length;
+//   console.log(objLength(lesson1));
+
+  // part 4
+
+  const objValue = (obj) => Object.values(obj);
+//   console.log(objValue(lesson1));
+
+  //part 5
+
+  const allLessons = Object.assign({}, {lesson1, lesson2, lesson3});
+  console.log(allLessons);
+
+//part 6
+
+const totalEstudantes = (allLessons) => {
+    const est1 = allLessons.lesson1.numeroEstudantes;
+    const est2 = allLessons.lesson2.numeroEstudantes;
+    const est3 = allLessons.lesson3.numeroEstudantes;
+    const total = est1 + est2 + est3;  
+    
+    return total;
+
+}
+// console.log(totalEstudantes(allLessons));
+
+//part 7
+
+const getValueByNumber = (obj, number) => Object.values(obj)[number];
+// console.log(getValueByNumber);
+
+
+
+
